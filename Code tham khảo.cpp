778f9ec6c9f6b7013fcfe606d41d6a9b7c15c609
AnhTuann5 <messerschmitt2006@gmail.com>
2025-06-18 16:02:50 +0700

    Create Code tham khảo.cpp

---
+++ Code tham khảo.cpp
@@ -0,0 +1,5 @@
+const std::string RESET = "\033[0m";
+const std::string RED = "\033[0;31m";
+const std::string GREEN = "\033[0;32m";
+const std::string BLUE = "\033[0;34m";
+const std::string YELLOW = "\033[0;33m";
